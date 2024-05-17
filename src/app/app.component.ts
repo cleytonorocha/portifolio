3b5b62a4ed5e140c9ed0929663c60500a7c15633
cleyton_orocha <cleytoodeoliveirarocha@gmail.com>
2024-05-17 18:10:26 -0300

    fix: fixed content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ApresentationComponent } from './main/apresentation.component';
+import { ApresentationComponent } from './apresentation/apresentation.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { TechComponent } from './tech/tech.component';
+import { ContactComponent } from './contact/contact.component';
+
 
 @Component({
   selector: 'app-root',
@@ -13,10 +15,11 @@ import { TechComponent } from './tech/tech.component';
     NavBarComponent,
     ApresentationComponent,
     ProjectsComponent,
-    TechComponent
+    TechComponent,
+    ContactComponent
   ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'portifolio';
