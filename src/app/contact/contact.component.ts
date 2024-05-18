c55b8c85a41369cff620423f7d9797e7d99ecb10
cleyton_orocha <cleytoodeoliveirarocha@gmail.com>
2024-05-17 21:24:29 -0300

    feat: contact component

---
+++ src/app/contact/contact.component.ts
@@ -0,0 +1,13 @@
+import { NgClass } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact',
+  standalone: true,
+  imports: [NgClass],
+  templateUrl: './contact.component.html',
+  styleUrl: './contact.component.css'
+})
+export class ContactComponent {
+
+}
