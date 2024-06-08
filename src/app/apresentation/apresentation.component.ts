465811cb201dbf35fe90ca19372997c101481071
cleyton_orocha <cleytoodeoliveirarocha@gmail.com>
2024-06-08 09:55:05 -0300

    fix: mobilie apresentation component

--- src/app/apresentation/apresentation.component.ts
+++ src/app/apresentation/apresentation.component.ts
@@ -9,4 +9,7 @@ import { Component } from '@angular/core';
 })
 export class ApresentationComponent {
 
+  firstParagraph: string = "Sou um desenvolvedor com amplo conhecimento da linguagem Java, abrangendo todos os conceitos básicos incluindo Orientação a Objetos e Programação Funcional. Tenho experiência em ORM com JDBC e Hibernate. Além disso, tenho familiaridade com Spring Framework, implementando testes unitários e TDD com JUnit e implementando padrões em projetos."
+  secoundParagraph: string = "As a back-end developer, I value the best architectural approach to enhance the application's performance, optimizing not only response speed but also system scalability and security."
+
 }
