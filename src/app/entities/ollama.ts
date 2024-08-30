530fa627b5e624d469bf4164c6d806b1c0054a1a
cleytonorocha <cleytoodeoliveirarocha@gmail.com>
2024-08-30 11:00:49 -0300

    feat: add ollamaai

---
+++ src/app/entities/ollama.ts
@@ -0,0 +1,19 @@
+import { Projects } from "../interfaces/Projects";
+
+export const ollama: Projects = {
+    title: "Ollama AI",
+    video : "",
+    thumbnailURL: "assets/projects-img/ollamaai/image1.png",
+    image: {
+        imageUrl: [
+            "assets/projects-img/ollamaai/image2.png",
+        ],
+        alt: "Implementação da Inteligência Artificial do Facebook"
+    },
+    shortExplication: "API de biblioteca com SPRING FRAMEWORK",
+    tech: ["JAVA", "Thymeleaf", "PROGRAMAÇÃO FUNCIONAL", "Ollama", "Spring-AI"],
+    github: "https://github.com/cleytonorocha/ollamaai",
+    deploy: "",
+    longText: "Esta aplicação utiliza Ollama AI integrada com o Spring Framework para fornecer respostas e consultas de usuários. O projeto é voltado para desenvolvedores que desejam criar aplicações com IA.",
+    id: "LibrarySystem"
+}
--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { jdbc } from '../entities/jdbc';
 import { librarySystem } from '../entities/library-system';
+import { ollama } from '../entities/ollama';
 import { stuffingTavern } from '../entities/stuffing-tavern';
 import { tddLibrary } from '../entities/tdd-library';
 import { Projects } from '../interfaces/Projects';
-import { CarouselModule } from 'ngx-bootstrap/carousel';
-import { jdbc } from '../entities/jdbc';
 
 
 @Component({
@@ -24,6 +25,7 @@ export class ProjectsComponent{
     librarySystem,
     tddLibrary,
     jdbc,
+    ollama
     // stuffingTavern,
     // stuffingTavern
 
